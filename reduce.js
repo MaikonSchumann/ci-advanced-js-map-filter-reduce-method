eab80bcd211910426a8082dfd43ea0a19e6fa6a2
MaikonSchumann <71983707+MaikonSchumann@users.noreply.github.com>
2024-04-05 08:06:12 +0100

    reduce method

--- reduce.js
+++ reduce.js
@@ -18,8 +18,39 @@ const teamMembers = [
     yrsExperience: 7,
   },
   {
-    name: "Andrew",
+    name: "Michael",
     profession: "Designer",
     yrsExperience: 1,
   },
+  {
+    name: "Kelly",
+    profession: "Designer",
+    yrsExperience: 3,
+  },
+  {
+    name: "Mark",
+    profession: "Manager",
+    yrsExperience: 10,
+  },
 ];
+
+// Totaling a specific object property
+let totalExperience = teamMembers.reduce(
+  (acc, curr) => acc + curr.yrsExperience,
+  0
+);
+console.log(totalExperience);
+
+// Grouping by a property, and totaling it too
+// {Developer: 12, Designer: 4} <-- This is what we want!
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(experienceByProfession);
